b0b5dbc1729f296be1ea72ebc76bbc4c1edaddb0
romainducrocq <romain.g.ducrocq@gmail.com>
2026-01-21 22:55:46 +0200

    add freebsd support to build scripts

--- build/cmake_build.sh
+++ build/cmake_build.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
-./build_preset.sh
+CC="gcc"
+CXX="g++"
+if [[ "$(uname -s)" == "Darwin"* ]]; then
+    CC="clang"
+    CXX="clang++"
+elif [[ "$(uname -s)" == "FreeBSD"* ]]; then
+    CC="clang"
+    CXX="clang++"
+fi
+
+./build_preset.sh ${CC}
 if [ ${?} -ne 0 ]; then exit 1; fi
 
 BUILD_CPP="OFF"
-export CC=$(which gcc)
+export CC=$(which ${CC})
 if [[ "${1}" == *"-cpp" ]]; then
     BUILD_CPP="ON"
-    export CXX=$(which g++)
-fi
-if [[ "$(uname -s)" == "Darwin"* ]]; then
-    export CC=$(which clang)
-    if [ "${BUILD_CPP}" = "ON" ]; then
-        export CXX=$(which clang++)
-    fi
+    export CXX=$(which ${CXX})
 fi
 
 cmake -G "Unix Makefiles" -S . -B release/ -DCMAKE_BUILD_TYPE=Release \
